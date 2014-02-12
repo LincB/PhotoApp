fcb04ab3714b2bb834e90e1522f4cc4cf9553284
zyedidia <zyedidia@gmail.com>
2014-02-12 11:09:36 -0500

    Added activity_overview

--- src/org/concordacademy/photoapp/Overview.java
+++ src/org/concordacademy/photoapp/Overview.java
@@ -1,7 +1,22 @@
 package org.concordacademy.photoapp;
 
+import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
 
 public class Overview extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_overview);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.overview, menu);
+		return true;
+	}
 
 }
