535e31748662017125db34777e05e4d4f4471d51
zyedidia <zyedidia@gmail.com>
2014-02-19 10:15:51 -0500

    Added programmatically generated imageview

--- src/org/concordacademy/photoapp/Overview.java
+++ src/org/concordacademy/photoapp/Overview.java
@@ -1,8 +1,13 @@
 package org.concordacademy.photoapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class Overview extends Activity {
 
@@ -10,6 +15,25 @@ public class Overview extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_overview);
+		
+		Bitmap bitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getPath() + "AppIcon57x57.png");
+		
+		// Create the ImageView
+	    ImageView image = new ImageView(this);
+
+	    // Layout Parameters
+	    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+	            FrameLayout.LayoutParams.MATCH_PARENT,
+	            FrameLayout.LayoutParams.MATCH_PARENT);
+
+	    image.setScaleType(ImageView.ScaleType.MATRIX);
+	    //image.setImageBitmap(bitmap);
+	    image.setImageResource(R.drawable.ic_launcher);
+	    //image.setOnTouchListener((OnTouchListener) this);    
+
+	    // Get the root layout and add our ImageView
+	    FrameLayout layout = (FrameLayout) findViewById(R.id.FrameLayout1);
+	    layout.addView(image, 0, params);
 	}
 
 	@Override
