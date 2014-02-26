1a101e625d1dd0aad60a53248f703c8e29e4a3e6
LincB <LincB@users.noreply.github.com>
2014-02-25 22:33:16 -0500

    Camera now saves, adds to file

--- src/org/concordacademy/photoapp/Overview.java
+++ src/org/concordacademy/photoapp/Overview.java
@@ -4,9 +4,12 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -16,6 +19,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
@@ -46,20 +50,19 @@ public class Overview extends Activity {
 				e.printStackTrace();
 			}
 		}
-				
+
 		try {
-			BufferedReader readFile = new BufferedReader(new InputStreamReader(openFileInput("photos.txt")));
+			FileInputStream inStream = openFileInput("photos.txt");
+			BufferedReader readFile = new BufferedReader(new InputStreamReader(inStream));
 			picturePaths = new ArrayList<String>();
 			String receiveString = "";
 			while ((receiveString = readFile.readLine()) != null) {
 				picturePaths.add(receiveString);
-				Log.wtf("picture", receiveString);
+				Log.i("picture", receiveString);
 				addImage(receiveString);
 				receiveString = "";
 			}
-			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -106,15 +109,15 @@ public class Overview extends Activity {
 			if (requestCode == 1) {
 				Uri selectedImage = data.getData();
 				String[] filePathColumn = { MediaStore.Images.Media.DATA };
-	
+
 				Cursor cursor = getContentResolver().query(selectedImage,
 						filePathColumn, null, null, null);
 				cursor.moveToFirst();
-	
+
 				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
 				String picturePath = cursor.getString(columnIndex);
 				cursor.close();
-	
+
 				try {
 					BufferedOutputStream toFile = new BufferedOutputStream(openFileOutput("photos.txt", MODE_APPEND));
 					toFile.write(picturePath.getBytes());
@@ -124,49 +127,65 @@ public class Overview extends Activity {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-	
+
 				addImage(picturePath);
 			} else if (requestCode == 2) {
-				//Uri selectedImage = data.getData();
-				
 				Bitmap photo = (Bitmap) data.getExtras().get("data");
-				
+				File photoFolder = new File(Environment.getExternalStorageDirectory(), "/");
+				photoFolder.mkdir();
+				Log.i(TAG, photoFolder.getAbsolutePath());
+				FileOutputStream out = null;
+				Calendar c = Calendar.getInstance();
+				String date = String.valueOf(c.get(Calendar.MONTH))
+						+ String.valueOf(c.get(Calendar.DAY_OF_MONTH))
+						+ String.valueOf(c.get(Calendar.YEAR))
+						+ String.valueOf(c.get(Calendar.HOUR_OF_DAY))
+						+ String.valueOf(c.get(Calendar.MINUTE))
+						+ String.valueOf(c.get(Calendar.SECOND));
+				File photoFileName = new File(photoFolder, date.toString() + ".jpg");
+				try
+				{
+					photoFileName.createNewFile();
+					out = new FileOutputStream(photoFileName);
+					photo.compress(Bitmap.CompressFormat.JPEG, 100, out);
+					out.flush();
+					out.close();
+					out = null;
+					MediaStore.Images.Media.insertImage(getContentResolver(), photo, photoFileName.toString() , "");
+					
+					BufferedOutputStream toFile = new BufferedOutputStream(openFileOutput("photos.txt", MODE_APPEND));
+					toFile.write(photoFileName.getAbsolutePath().getBytes());
+					toFile.write('\n');
+					toFile.flush();
+					toFile.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 				addImage(photo);
-				
-				/*String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-				Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-				cursor.moveToFirst();
-
-				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-				//file path of captured image
-				String filePath = cursor.getString(columnIndex); 
-				Log.i(TAG, filePath);
-				cursor.close();*/
 			}
-			
+
 		}
 
 	}
-	
+
 	public void addImage(String fileName) {
-		
+
 		LayoutInflater inflater = LayoutInflater.from(this);
 		GridLayout container = (GridLayout) findViewById(R.id.GridLayout1);
 		ImageView imageView = (ImageView) inflater.inflate(R.layout.photo_frame,null);
 		imageView.setImageBitmap(BitmapFactory.decodeFile(fileName));
-					
+
 		imageView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				onImageClick(arg0);
 			}
-        });
-		
+		});
+
 		if (numImages > 5) {
-			
+
 		}
-		
+
 		imageView.setVisibility(0);
 		GridLayout.LayoutParams layout = new GridLayout.LayoutParams();
 		layout.setGravity(Gravity.LEFT | Gravity.TOP);
@@ -175,27 +194,27 @@ public class Overview extends Activity {
 		layout.setMargins(5, 5, 5, 5);
 		layout.columnSpec = GridLayout.spec(0);
 		container.addView(imageView,layout);
-		
+
 		numImages += 1;
 	}
-	
+
 	public void addImage(Bitmap b) {
 		LayoutInflater inflater = LayoutInflater.from(this);
 		GridLayout container = (GridLayout) findViewById(R.id.GridLayout1);
 		ImageView imageView = (ImageView) inflater.inflate(R.layout.photo_frame,null);
 		imageView.setImageBitmap(b);
-					
+
 		imageView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				onImageClick(arg0);
 			}
-        });
-		
+		});
+
 		if (numImages > 5) {
-			
+
 		}
-		
+
 		imageView.setVisibility(0);
 		GridLayout.LayoutParams layout = new GridLayout.LayoutParams();
 		layout.setGravity(Gravity.LEFT | Gravity.TOP);
@@ -204,10 +223,10 @@ public class Overview extends Activity {
 		layout.setMargins(5, 5, 5, 5);
 		layout.columnSpec = GridLayout.spec(0);
 		container.addView(imageView,layout);
-		
+
 		numImages += 1;
 	}
-	
+
 	public void onImageClick(View v) {
 		Intent intent = new Intent(getBaseContext(), PhotoActivity.class);
 
